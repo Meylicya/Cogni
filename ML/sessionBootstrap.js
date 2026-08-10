5de81d34cf4a2ed066903e51567f42b21ad799cd
void <leesofa@icloud.com>
2026-08-10 03:50:55 +0300

    Remove unnecessary 'git' line from sessionBootstrap.js

--- ML/sessionBootstrap.js
+++ ML/sessionBootstrap.js
@@ -2,7 +2,6 @@ import { getPatientSessionContext } from './patientSessionContext.js';
 import { ZPDEngine } from './zpdEngine.js';
 import { SymptomCheckinScorer } from './scorer.js';
 
-git 
 async function startPatientSession(patientId) {
   const { difficultyTier, languageSymptomsFlagged } = await getPatientSessionContext(patientId);
 
