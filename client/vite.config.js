23c85ff492ffcebeaf043b00da6947e56745190a
void <leesofa@icloud.com>
2026-08-18 23:10:32 +0300

    included packages for AI models and saved the original code

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,3 +1,7 @@
+
+/*
+I'll keep the original commented in case if something will go wrong.
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -8,5 +12,22 @@ export default defineConfig({
     fs: {
       strict: false // This allows App.jsx to read your files from the Dashboards folder!
     }
+  }
+})
+*/
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    fs: {
+      strict: false // This allows App.jsx to read your files from the Dashboards folder!
+    }
+  },
+  optimizeDeps: {
+    include: ['@mediapipe/tasks-vision', '@ricky0123/vad-web']
   }
 })
